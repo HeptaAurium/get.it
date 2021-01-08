62a674ec03da04be85d1948793b5a6920f5de036
Ngugi Kiarie <ngugithe4th@gmail.com>
2021-01-08 14:39:24 +0300

    ha

--- resources/views/layouts/extras/css.blade.php
+++ resources/views/layouts/extras/css.blade.php
@@ -3,8 +3,11 @@
    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
    <link rel="preconnect" href="https://fonts.gstatic.com">
    <link href="https://fonts.googleapis.com/css2?family=Redressed&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
 
    <!-- Styles -->
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+   {{-- Favicon --}}
+   <link rel="shortcut icon" href="{{asset('img/icons/logo-clear.png')}}" type="image/x-icon">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,100 +2,86 @@
 @section('title', 'Homepage | ')
 
 @section('content')
-    <div class="showcase-universal">
-        <div class="scroll-buttons">
-            <button class="btn btnNext flex-center">
-                <i class="fa fa-angle-right" aria-hidden="true"></i>
-            </button>
-            <button class="btn btnPrev flex-center">
-                <i class="fa fa-angle-left" aria-hidden="true"></i>
-            </button>
-        </div>
-        <div class="indicators">
-            <ol style="list-style-type: none" class="d-flex flex-row">
-                <li class="li-indicator">
-                    <div class="row align-items-center">
-                        <img class="col-4 rounded-circle img-fluid" src="{{asset('img/front/100.jpg')}}" alt="">
-                        <div class="col-8">
-                            <div class="flex-column">
-                                <h3 class="text-center">PRODUCT NAME</h3>
-                                <h6 class="text-center font-weight-bolder">KES 5,000</h6>
+    @include('home.showcase')
+
+    <div class="col-12">
+        <div class="products padding">
+            <div class="arrivals">
+                <h2 class="display-3 prod-header">New Arrivals</h2>
+                <div class="py-5 px-5">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-3">
+                            <div class="card" style="background:url('{{ asset('img/catalogue/1004.jpg') }}')">
+                                <div class="product align-items-center p-2 text-center">
+                                    {{-- <img src="{{ asset('img/catalogue/1001.jpg') }}"
+                                        alt="" class="rounded w-100"> --}}
+                                    <h5 class="prod-name">Product Name</h5>
+                                    <div class="mt-3 info">
+                                        <span class="text desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit.</span>
+                                        <span class="text desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit.</span>
+                                    </div>
+                                    <div class="cost mt-3 text-light">
+                                        <span>KSH 2,000</span>
+                                        <div class="stars">
+                                            @for ($i = 0; $i < 5; $i++)
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                            @endfor
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-md-3">
+                            <div class="card" style="background:url('{{ asset('img/catalogue/1006.jpg') }}')">
+                                <div class="product align-items-center p-2 text-center">
+                                    {{-- <img src="{{ asset('img/catalogue/1001.jpg') }}"
+                                        alt="" class="rounded w-100"> --}}
+                                    <h5 class="prod-name">Product Name</h5>
+                                    <div class="mt-3 info">
+                                        <span class="text desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit.</span>
+                                        <span class="text desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit.</span>
+                                    </div>
+                                    <div class="cost mt-3 text-light">
+                                        <span>KSH 2,000</span>
+                                        <div class="stars">
+                                            @for ($i = 0; $i < 5; $i++)
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                            @endfor
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-md-3">
+                            <div class="card" style="background:url('{{ asset('img/catalogue/1005.jpg') }}')">
+                                <div class="product align-items-center p-2 text-center">
+                                    {{-- <img src="{{ asset('img/catalogue/1001.jpg') }}"
+                                        alt="" class="rounded w-100"> --}}
+                                    <h5 class="prod-name">Product Name</h5>
+                                    <div class="mt-3 info">
+                                        <span class="text desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit.</span>
+                                        <span class="text desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                                            elit.</span>
+                                    </div>
+                                    <div class="cost mt-3 text-light">
+                                        <span>KSH 2,000</span>
+                                        <div class="stars">
+                                            @for ($i = 0; $i < 5; $i++)
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                            @endfor
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </li>
-                <li class="li-indicator">
-                    <div class="row align-items-center">
-                        <img class="col-4 rounded-circle img-fluid" src="{{asset('img/front/1001.jpg')}}" alt="">
-                        <div class="col-8">
-                            <div class="flex-column">
-                                <h3 class="text-center">PRODUCT NAME</h3>
-                                <h6 class="text-center font-weight-bolder">KES 5,000</h6>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li class="li-indicator">
-                    <a href="#" class="btn">
-                    <div class="row align-items-center">
-                        <img class="col-4 rounded-circle img-fluid" src="{{asset('img/front/1002.jpg')}}" alt="">
-                        <div class="col-8">
-                            <div class="flex-column">
-                                <h3 class="text-center">PRODUCT NAME</h3>
-                                <h6 class="text-center font-weight-bolder">KES 5,000</h6>
-                            </div>
-                        </div>
-                    </div>
-                    </a>
-                </li>
-            </ol>
+                </div>
+            </div>
         </div>
 
-        <div class="showcase-outer one active">
-            <div class="showcase">
-                <div class="nav">
-                    @include('home.nav')
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="content d-flex flex-column">
-                                    <h1 class='non-diplay'>SNEAKERS</h1>
-                                    <h3 class="display display-4">Kicks for Cliques</h3>
-                                    <P>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus porro eveniet
-                                        cupiditate suscipit commodi ipsam, rem illum necessitatibus atque, qui perferendis
-                                        laboriosam eum nesciunt reprehenderit? Id nostrum iusto eligendi quidem.
-                                    </P>
-                                    <button class="btn btn-lg">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="showcase-outer two">
-            <div class="showcase">
-                <div class="nav">
-                    @include('home.nav')
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="content d-flex flex-column">
-                                    <h1 class='non-diplay'>KITCHEN WARE</h1>
-                                    <h3 class="display display-4">Style up your Kitchen</h3>
-                                    <P>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus porro eveniet
-                                        cupiditate suscipit commodi ipsam, rem illum necessitatibus atque, qui perferendis
-                                        laboriosam eum nesciunt reprehenderit? Id nostrum iusto eligendi quidem.
-                                    </P>
-                                    <button class="btn btn-lg">Get Started</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+    @endsection
